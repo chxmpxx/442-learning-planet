cdb38058bf3b77768f53515e37d0a9d972920c97
ThanakornDonklang <thanakorn.do@ku.th>
2021-08-05 23:04:59 +0700

    edit reward api

--- src/store/RewardApi.js
+++ src/store/RewardApi.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import Axios from "axios";
-// import AuthService from "@/services/AuthService";
+import AuthService from "@/services/AuthService";
 
 let api_endpoint = process.env.VUE_APP_USER_ENDPOINT || "http://localhost:1337";
 
@@ -21,15 +21,26 @@ export default new Vuex.Store({
     add(state, { payload }) {
       state.data.push(payload);
     },
+    edit(state, { payload }) {
+      state.data[payload.index].name = payload.name;
+      state.data[payload.index].point = payload.point;
+      state.data[payload.index].image = payload.image;
+      state.data[payload.index].amount = payload.amount;
+    },
   },
   actions: {
     async fetchReward({ commit }) {
-      // let headers = AuthService.getApiHeader;
-      let res = await Axios.get(api_endpoint + "/rewards");
+      let headers = AuthService.getApiHeader();
+      let res = await Axios.get(api_endpoint + "/rewards", headers);
       commit("fetch", { res });
     },
     addReward({ commit }, payload) {
       commit("add", { payload });
+    },
+    editReward({ commit }, payload) {
+      let headers = AuthService.getApiHeader();
+      let res = await Axios.put(api_endpoint + "/rewards", headers);
+      commit("edit", { payload });
     },
   },
   modules: {},
