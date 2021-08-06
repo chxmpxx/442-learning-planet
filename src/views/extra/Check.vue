8424fe5df39e77660c7d4dc9aa3100b03f4c2a25
Arreeya <arreeya.su@ku.th>
2021-08-06 18:57:19 +0700

    add style to Check and Wait pages

--- src/views/extra/Check.vue
+++ src/views/extra/Check.vue
@@ -1,26 +1,42 @@
 <template>
-    <div>
+    <div class='check'>
+        <NavBar />
+        <br><br><br><br><br><br>
         <check :id="this.id"/>
     </div>
 </template>
 
 <script>
-    import Check from '../../components/extra/Check'
-    export default {
-        components:{
-            Check
-        },
-        data(){
-            return{
-                id:''
-            }
-        },
-        created(){
-            this.id = this.$route.params.id
+import NavBar from "@/components/NavBar.vue";
+import Check from '../../components/extra/Check'
+export default {
+    components:{
+        NavBar,
+        Check
+    },
+    data(){
+        return{
+            id:''
         }
+    },
+    created(){
+        this.id = this.$route.params.id
     }
+}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.check{
+    font-family: 'Prompt';
+    font-weight: normal;
+    font-style: normal;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.2)),
+        url("../../assets/background.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
 </style>
